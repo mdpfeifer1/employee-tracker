7ecc982f39a8ba4f7f8f4649bd2da67ea8c374b0
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-10-22 15:21:37 -0500

    pulled readme from challenge for an ordered to-dos/created seeds.sql to prepopulate

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,39 @@
+DROP DATABASE IF EXISTS employee_db;
+
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
+-- Creates the table "produce" within inventory_db --
+CREATE TABLE employee (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT NOT NULL,
+  -- Makes a string column called "name" which cannot contain null --
+  name VARCHAR(100) NOT NULL
+);
+
 SELECT DATABASE();
+
+
+-- * `department`
+--     * `id`: `INT PRIMARY KEY`
+--     * `name`: `VARCHAR(30)` to hold department name
+
+-- * `role`
+--     * `id`: `INT PRIMARY KEY`
+--     * `title`: `VARCHAR(30)` to hold role title
+--     * `salary`: `DECIMAL` to hold role salary
+--     * `department_id`: `INT` to hold reference to department role belongs to
+
+-- * `employee`
+--     * `id`: `INT PRIMARY KEY`
+--     * `first_name`: `VARCHAR(30)` to hold employee first name
+--     * `last_name`: `VARCHAR(30)` to hold employee last name
+--     * `role_id`: `INT` to hold reference to employee role
+--     * `manager_id`: `INT` to hold reference to another employee that is the manager of the current employee 
+--     (`null` if the employee has no manager)
+
+You might want to use a separate file that contains functions for performing specific SQL 
+queries you'll need to use. A constructor function or class could be helpful for organizing these. 
+You might also want to include a `seeds.sql` file to 
+pre-populate your database, making the development of individual features much easier.
